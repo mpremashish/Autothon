897e8ebbd0a9acba92f38da8eb2c7484fca9e4e1
gulyani, shweta <sgulyani@cvent.com>
2019-04-25 12:51:23 +0530

    added rest assured api call

--- TestAutothon/testautothon-18/TestAutothon-2018/src/main/java/JsonCollector/JsonCollector.java
+++ TestAutothon/testautothon-18/TestAutothon-2018/src/main/java/JsonCollector/JsonCollector.java
@@ -1,45 +1,67 @@
 package JsonCollector;
 
-import io.restassured.response.Response;
-import io.restassured.response.ResponseBodyExtractionOptions;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import cucumber.api.java.it.Ma;
+import twitter4j.*;
+import twitter4j.conf.*;
 
-import static io.restassured.RestAssured.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Stack;
+import java.util.Map;
 
+public class JsonCollectorJava {
+    public static void main(String[] args) {
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setOAuthConsumerKey("3QHsalYXMeQchpOhwZWc2HfrC");
+        cb.setOAuthConsumerSecret("sDE8sk6kj0aegjy2fLIZXV5oZRZY5AOH6XcX1LcyYP0tjjNqXT");
+        cb.setOAuthAccessToken("1121272893663584256-RoLkgcGZs7caL56l4YrCu9QZBVbmfD");
+        cb.setOAuthAccessTokenSecret("7eKt0SbuW0pVzD2F9TTXyrYVDD9bFH9RsUHKIg8yRGemO");
+        TwitterFactory tf = new TwitterFactory(cb.build());
+        Twitter twitter = tf.getInstance();
+        ObjectMapper mapper=new ObjectMapper();
 
-public class JsonCollector {
+        Paging paging = new Paging(1, 50);
+        int retweethighest=0;
+        String retweet = "";
+        HashMap<String,Integer> hashtag = new HashMap<>();
+        int Likehighest=0;
+        try {
+            List<Status> statuses = twitter.getUserTimeline(
+                    "stepin_forum", paging);
+            int count=0;
+            for (Status status : statuses) {
+                count=count+1;
+                if (retweethighest<status.getRetweetCount()){
+                    retweethighest=status.getRetweetCount();
+                    retweet=status.getText();
+                }
+                if (Likehighest<status.getFavoriteCount()){
+                    Likehighest=status.getFavoriteCount();
+                }
+                for (HashtagEntity h:status.getHashtagEntities()){
+                    if (hashtag.containsKey(h.getText())){
+                        int value =hashtag.get(h.getText())+1;
+                        hashtag.put(h.getText(),value);
+                    }
+                    if(h.getText().toLowerCase().matches("[a-zA-Z0-9_]*$")&&h.getText().toLowerCase().matches(".*\\S.*")){
+                        System.out.println(h.getText());
+                        hashtag.put(h.getText(),1);
+                    }
+                }
 
-    private static List<Integer> findFibonacci(int num) {
-        List<Integer> al = new ArrayList<>();
-        int a = 0;
-        int b = 1;
-        int next;
-        al.add(a);
-        al.add(b);
-        for (int i = 0; i < num - 2; i++) {
-            next = a + b;
-            al.add(next);
-            a = b;
-            b = next;
+            }
+            System.out.println(count);
+            System.out.println(retweethighest);
+            System.out.println(Likehighest);
+            for (Set : hashtag.entrySet()){
+            }
+        } catch (TwitterException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+
         }
-        return al;
-    }
-
-    public static Stack<JsonDataObject> findJsonObjects() {
-        Stack<JsonDataObject> jsonDataObjects = new Stack<JsonDataObject>();
-        Response response = given().get("http://jsonplaceholder.typicode.com/posts");
-        List<Integer> userId = response.jsonPath().getList("userId");
-        List<Integer> id = response.jsonPath().getList("id");
-        List<String> title = response.jsonPath().getList("title");
-        List<String> body = response.jsonPath().getList("body");
-
-        findFibonacci(10).forEach(i -> jsonDataObjects.push(new JsonDataObject(userId.get(i), id.get(i),
-                title.get(i), body.get(i))));
-
-        return jsonDataObjects;
-
     }
 }
